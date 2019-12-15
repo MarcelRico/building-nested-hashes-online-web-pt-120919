a08e975e4d09ca45e3963bde3a7cd17bd3b782f0
Marcel Rico <personal.mr4@gmail.com>
2019-12-15 13:03:31 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -8,16 +8,16 @@ end
 def second_challenge
   epic_tragedy = {
     :montague =>{
-      :patriarch => "",
-      :matriarch => "",
-      :hero => "",
-      :hero_friends => ""
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
     },
     :capulet =>{
-      :patriarch => "",
-      :matriarch => "",
-      :hero => "",
-      :heroine_friends => ""
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :heroine_friends => []
     }
   }
 end
